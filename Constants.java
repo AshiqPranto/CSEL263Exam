d17bc3f114adc2618c94af1b7416d7bb27ae6ed9
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2021-12-06 08:19:32 +0600

    task#5 done && replaced String literals with constants

---
+++ Constants.java
@@ -0,0 +1,11 @@
+public class Constants{
+    final String StudentsList = "students.txt";
+    final String ShowAll = "-a";
+    final String ShowRandom = "-r";
+    final String ShowCount = "-c";
+    final String FindEntry = "?";
+    final String AddEntry = "+";
+    final String StudentEntryDelimiter = ",";
+}
+
+
--- StudentList.java
+++ StudentList.java
@@ -2,67 +2,69 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 public class StudentList {
-
+	
+	static Constants constants = new Constants();
+	
 	public static BufferedReader getBufferedReader()
 	{
 		// BufferedReader bufferedReader;
 		try {
 			BufferedReader bufferedReader = new BufferedReader(
 				new InputStreamReader(
-					new FileInputStream("students.txt")));
-			return bufferedReader;
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public static BufferedWriter getBufferedWriter()
-	{
-		try {
-			BufferedWriter bufferedWriter = new BufferedWriter(
-				new FileWriter("students.txt", true));
-			return bufferedWriter;
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		return null;
-	}
-
-	public static void main(String[] args) {
-//		Check arguments
-		if(args.length!=1)
-		{
-			System.out.println("Please Enter only one argument..!");
-		}
-		else if(args[0].equals("a")) {
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader bufferedReader = getBufferedReader();
-			String readString = bufferedReader.readLine();
-			String studentList[] = readString.split(",");			
-			for(String student : studentList)
-			{
-				System.out.println(student);
+					new FileInputStream(constants.StudentsList)));
+					return bufferedReader;
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+				return null;
 			}
-			}catch (Exception e)
+			
+			public static BufferedWriter getBufferedWriter()
 			{
+				try {
+					BufferedWriter bufferedWriter = new BufferedWriter(
+						new FileWriter(constants.StudentsList, true));
+						return bufferedWriter;
+					} catch (Exception e) {
+						System.out.println(e);
+					}
+					return null;
+				}
 				
-			} 
-			System.out.println("Data Loaded.");
-		}
-		else if(args[0].equals("r")) 
-		{
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader bufferedReader = getBufferedReader();
-			String readString = bufferedReader.readLine();
-			// System.out.println(r);
-			String studentList[] = readString.split(", ");	
-			// Random x = new Random();
-			// 	int y = x.nextInt();
-			// 		System.out.println(i[y]);
-			int randomNumber =  (int)((double)Math.random()*(int)studentList.length);
+				public static void main(String[] args) {
+					//		Check arguments
+					if(args.length!=1)
+					{
+						System.out.println("Please Enter only one argument..!");
+					}
+					else if(args[0].equals(constants.ShowAll)) {
+						System.out.println("Loading data ...");			
+						try {
+							BufferedReader bufferedReader = getBufferedReader();
+							String readString = bufferedReader.readLine();
+							String studentList[] = readString.split(constants.StudentEntryDelimiter);			
+							for(String student : studentList)
+							{
+								System.out.println(student);
+							}
+						}catch (Exception e)
+						{
+							
+						} 
+						System.out.println("Data Loaded.");
+					}
+					else if(args[0].equals(constants.ShowRandom)) 
+					{
+						System.out.println("Loading data ...");			
+						try {
+							BufferedReader bufferedReader = getBufferedReader();
+							String readString = bufferedReader.readLine();
+							// System.out.println(r);
+							String studentList[] = readString.split(constants.StudentEntryDelimiter);	
+							// Random x = new Random();
+							// 	int y = x.nextInt();
+							// 		System.out.println(i[y]);
+							int randomNumber =  (int)((double)Math.random()*(int)studentList.length);
 			System.out.println(studentList[randomNumber]);
 			} catch (Exception e)
 			{
@@ -70,7 +72,7 @@ public class StudentList {
 			} 
 			System.out.println("Data Loaded.");			
 		}
-		else if(args[0].contains("+")){
+		else if(args[0].contains(constants.AddEntry)){
 			System.out.println("Loading data ...");			
 			try {
 			BufferedWriter bufferedWriter = getBufferedWriter();
@@ -79,7 +81,7 @@ public class StudentList {
 	        String dateFormatSample = "dd/mm/yyyy-hh:mm:ss a";
 	        DateFormat dateFormat = new SimpleDateFormat(dateFormatSample);
 	        String finalDate = dateFormat.format(date);
-			bufferedWriter.write(", "+newStudent+"\nList last updated on "+finalDate);
+			bufferedWriter.write(constants.StudentEntryDelimiter+newStudent+"\nList last updated on "+finalDate);
 			bufferedWriter.close();
 			} catch (Exception e)
 			{
@@ -87,13 +89,13 @@ public class StudentList {
 			}		
 			System.out.println("Data Loaded.");	
 		}
-		else if(args[0].contains("?")) 
+		else if(args[0].contains(constants.FindEntry)) 
 		{
 			System.out.println("Loading data ...");			
 			try {
 			BufferedReader bufferedReader = getBufferedReader();
 			String readString = bufferedReader.readLine();
-			String studentList[] = readString.split(",");	
+			String studentList[] = readString.split(constants.StudentEntryDelimiter);	
 			boolean done = false;
 			String searchStudent = args[0].substring(1);
 			for(int idx = 0; idx<studentList.length && !done; idx++) 
@@ -110,7 +112,7 @@ public class StudentList {
 			} 
 			System.out.println("Data Loaded.");				
 		}
-		else if(args[0].contains("c")) 
+		else if(args[0].contains(constants.ShowCount)) 
 		{
 			System.out.println("Loading data ...");			
 			try {
